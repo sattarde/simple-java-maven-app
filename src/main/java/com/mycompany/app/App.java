55f36f3f160ec92dc4873c31cb54a7775148e05d
sub111 <39374754+sub111@users.noreply.github.com>
2018-05-30 21:25:05 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,11 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        int target = -5;
+        int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
     }
 
     private final String getMessage() {
